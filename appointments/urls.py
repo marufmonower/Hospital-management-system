4fcb0fe3aa54ca10662dea84f0d49abd58102216
Maruf Monower <marufmonower@gmail.com>
2025-04-27 11:17:19 +0600

    make_working_templates

--- appointments/urls.py
+++ appointments/urls.py
@@ -4,4 +4,5 @@ from .import views
 urlpatterns = [
     path('schedule/',views.schedule_appointment,name = 'schedule_appointment'),
     path('<int:appointment_id>/',views.appontment_detail,name = 'appontment_detail'),
+    path('',views.appointment_list,name='appointment_list'),
 ]
